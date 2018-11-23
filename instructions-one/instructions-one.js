f37495ff782fd73504ca02edff39a0ebf8478931
codchunks79 <45175219+codchunks79@users.noreply.github.com>
2018-11-23 11:51:23 +0000

    Add files via upload
    Second iteration into Cols 23/11/18

--- instructions-one/instructions-one.js
+++ instructions-one/instructions-one.js
@@ -4,18 +4,28 @@ Vue.component('button-counter', {
       counter: 0
     }
   },
-  template: '<div class="col-xs-12 col-md-3 offset-md-1 card content text-center"><div class="form row"><div class="form-lock-up"><div class="value-button-down" v-on:click="counter --"><i class="fas fa-sort-down"></i></div><p class="input">{{counter}}</p><div class="value-button-up" v-on:click="counter++"><i class="fas fa-sort-up"></i></button></div></div></div></div>',
+  template: '<div class="col-xs-12 col-md-2 card content text-center"><div class="form row"><div class="innerCard"><div class="form-lock-up"><div class="value-button-down" v-on:click="counter --"><i class="fas fa-sort-down"></i></div><p class="input">{{counter}}</p><div class="value-button-up" v-on:click="counter++"><i class="fas fa-sort-up"></i></button></div></div></div></div></div>',
 });
 
 Vue.component('header-nav', {
 
-  template: '<header class="container-fluid headerWrapper"><nav class="navbar navbar-light bg-light"><img src="lifetime-logo-e1527083477978.gif" alt="lifetime_logo"></nav></header>',
+  template: '<header class="container-fluid headerWrapper"><nav class="navbar navbar-light bg-light"><img src="lifetime-logo-e1527083477978.gif" alt="lifetime_logo"><h2>Name and Instructions</h2></nav></header>',
 });
 
 Vue.component('name-instructions', {
   data: function () {
     return {
       title: "Name",
+      instruct: "Instructions",
+    }
+  },
+  template: '<section class="container-fluid wrapperTwo"><div class="row"><div class="col-md-8 d-none d-md-block noPadding"><h2>{{title}}</h2></div><div class="col-md-3 offset-sm-1 d-none d-md-block noPadding"><h2>{{instruct}}</h2></div></div></section>',
+});
+
+Vue.component('name-instructions-two', {
+  data: function () {
+    return {
+      title: "Name / ",
       instruct: "Instructions",
     }
   },
